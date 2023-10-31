1df1fd205da09a98bec6f1bf154839e7ad312e13
jihooDev <wlgn829@kakao.com>
2023-10-31 09:27:16 +0900

    feat: 이미지 추가 / Column 위젯

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,34 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-      home: Scaffold(
-    backgroundColor: Colors.green,
-    body: Center(
-      child: Text(
-        '안녕하세요',
-        style: TextStyle(color: Colors.white, fontSize: 20.0),
-      ),
+  runApp(
+    MaterialApp(
+      home: HomeScreen(),
     ),
-  )));
+  );
+}
+
+class HomeScreen extends StatelessWidget {
+  // hot reload는 빌드 함수에 있는 코드만 재실행을 한다.
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Column(
+        // Column의 위치를 조정할 때
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Image.asset(
+              'asset/logo.png',
+              width: 150,
+            ),
+          ),
+          CircularProgressIndicator(
+            color: Colors.white,
+          )
+        ],
+      ),
+    );
+  }
 }
